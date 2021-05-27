46b06ddc87178a29884bbec01d0d1bd38ba43fb2
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-27 16:15:08 -0400

    updated test and updated token attributes

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,5 +1,13 @@
 pragma solidity ^0.5.0;
 
 contract Token {
-     string public name = "StarLord";
+     string public name = "Romulon Token";
+     string public symbol = "ROM";
+     uint256 public decimals = 18;
+     uint256 public totalSupply;
+
+     constructor() public {
+          totalSupply = 1000000 * (10 ** decimals);
+     }
+
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -5,17 +5,41 @@ require('chai')
 .should()
 
 contract('Token', (accounts) => {
+    const name = "Romulon Token"
+    const symbol = "ROM"
+    const decimals = "18"
+    const totalSupply = "1000000000000000000000000"
+    let token 
+
+
+    beforeEach(async () => {
+        token = await Token.new()
+    })
 
     describe('deployment', () => {
         it('tracks the name', async () => {
-            const token = await Token.new()
             const result = await token.name()
-            result.should.equal('StarLord')
+            result.should.equal(name)
             //Read token name here...
-            //Token name is starlord
+            //Token name is Romulon
             //fetch token from blockchain
             //Read token name
-            //Check token name is starlord
+            //Check token name is Romulon
+        })
+
+        it('tracks the symbol', async () => {
+            const result = await token.symbol()
+            result.should.equal(symbol)
+        })
+
+        it('tracks the decimals', async () => {
+            const result = await token.decimals()
+            result.toString().should.equal(decimals)
+       })
+
+        it('tracks the total supply', async () => {
+            const result = await token.totalSupply()
+            result.toString().should.equal(totalSupply)
         })
     })
 })
