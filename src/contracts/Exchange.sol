39ae5f8318823993a26347b5655aeb66b867e5f4
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-30 12:42:20 -0400

    added exchange smart contract and test, wrote out inital deployment test and so far set the fee account in the constructor and the test passes

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,28 @@
+pragma solidity ^0.5.0;
+
+contract Exchange {
+
+    address public feeAccount; //account that recieves exchange fees
+
+    constructor (address _feeAccount) public {
+        feeAccount = _feeAccount; //state variable set equal to the local variable
+    }
+}
+
+
+
+
+//Deposit & Withdraw
+
+
+
+
+
+//Manage Orders
+
+
+
+
+
+
+//Handle Trades
---
+++ test/Exchange.test.js
@@ -0,0 +1,28 @@
+import { tokens, EVM_REVERT } from "./helpers"
+
+const Exchange = artifacts.require('./Exchange')
+
+require('chai')
+    .use(require('chai-as-promised'))
+    .should()
+
+contract('Exchange', ([deployer, feeAccount]) => {
+    let exchange
+
+
+    beforeEach(async () => {
+        exchange = await Exchange.new(feeAccount)
+    })
+
+    describe('deployment', () => {
+        it('tracks the fee account', async () => {
+            const result = await exchange.feeAccount()
+            result.should.equal(feeAccount)
+           
+        })
+
+       
+    })
+
+
+})
