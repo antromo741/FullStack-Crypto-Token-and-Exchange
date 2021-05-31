5152569f57bca3be3f4377c9d8a4d655cca93ab0
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-31 15:19:43 -0400

    added deposit event

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -5,9 +5,13 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Exchange {
     using SafeMath for uint;
 
+    //Variables
     address public feeAccount; //account that recieves exchange fees
     uint256 public feePercent; //THe fee percentage
     mapping(address => mapping(address => uint256)) public tokens;
+
+    //Events
+    event Deposit(address token, address user, uint256 amount, uint256 balance);
 
     constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount; //state variable set equal to the local variable
@@ -17,7 +21,7 @@ contract Exchange {
     function depositToken(address _token, uint256 _amount) public {
         require(Token(_token).transferFrom(msg.sender, address(this), _amount));
         tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
-    
+        emit Deposit(_token, msg.sender, _amount, tokens[_token][msg.sender]);
     }
 
 
