21c590b1a3229cabbd2c86e29773b674f644eec3
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-31 15:14:42 -0400

    added tracking the token tests, and a transfer function to deal with updating the amounts properly

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,20 +1,22 @@
 pragma solidity ^0.5.0;
 import "./Token.sol";
-
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Exchange {
+    using SafeMath for uint;
 
     address public feeAccount; //account that recieves exchange fees
     uint256 public feePercent; //THe fee percentage
-   
+    mapping(address => mapping(address => uint256)) public tokens;
+
     constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount; //state variable set equal to the local variable
         feePercent = _feePercent;
     }
 
     function depositToken(address _token, uint256 _amount) public {
-        Token(_token).transferFrom(msg.sender, address(this), _amount);
-    
+        require(Token(_token).transferFrom(msg.sender, address(this), _amount));
+        tokens[_token][msg.sender] = tokens[_token][msg.sender].add(_amount);
     
     }
 
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -51,6 +51,9 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
                 let balance
                 balance = await token.balanceOf(exchange.address)
                 balance.toString().should.equal(amount.toString())
+                // check tokens on exchange
+                balance = await exchange.tokens(token.address, user1)
+                balance.toString().should.equal(amount.toString())
             })
         })
 
