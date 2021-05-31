28bf9b100132a99d32da3a941220bed7b50a4007
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-05-31 15:01:34 -0400

    wrote tests for token depositing, and added a deposit token function

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,13 +1,24 @@
 pragma solidity ^0.5.0;
+import "./Token.sol";
+
 
 contract Exchange {
 
     address public feeAccount; //account that recieves exchange fees
     uint256 public feePercent; //THe fee percentage
+   
     constructor (address _feeAccount, uint256 _feePercent) public {
         feeAccount = _feeAccount; //state variable set equal to the local variable
         feePercent = _feePercent;
     }
+
+    function depositToken(address _token, uint256 _amount) public {
+        Token(_token).transferFrom(msg.sender, address(this), _amount);
+    
+    
+    }
+
+
 }
 
 
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,17 +1,27 @@
 import { tokens, EVM_REVERT } from "./helpers"
 
+const Token  = artifacts.require('./Token')
 const Exchange = artifacts.require('./Exchange')
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, user1]) => {
+    let token
     let exchange
     const feePercent = 10
 
     beforeEach(async () => {
-        exchange = await Exchange.new(feeAccount, feePercent)
+        //deploy token
+        token = await Token.new()
+        
+        
+        //transfer token to user 1
+       token.transfer(user1, tokens(100), { from: deployer })
+
+        //deploy exchange
+         exchange = await Exchange.new(feeAccount, feePercent)
     })
 
     describe('deployment', () => {
@@ -23,6 +33,29 @@ contract('Exchange', ([deployer, feeAccount]) => {
         it('tracks the fee percent', async () => {
             const result = await exchange.feePercent()
             result.toString().should.equal(feePercent.toString())
+        })
+    })
+    
+    describe('depositing tokens', () => { 
+        let result
+        let amount
+
+        beforeEach(async () => {
+            amount = tokens(10)
+            await token.approve(exchange.address, amount, {from: user1 })
+            const result = await exchange.depositToken(token.address, amount, {from: user1})
+        })
+        
+        describe('success', () => {
+            it('tracks the token deposit', async () => {
+                let balance
+                balance = await token.balanceOf(exchange.address)
+                balance.toString().should.equal(amount.toString())
+            })
+        })
+
+        describe('failure', () => {
+
         })
     })
 
