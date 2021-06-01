baee747475acc9ba5e7be3e1dc235d8768cc078f
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-01 17:41:46 -0400

    made a make order function

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,18 +11,28 @@ contract Exchange {
     address constant ETHER = address(0); //store ether in tokens mapping with blank addess
     mapping(address => mapping(address => uint256)) public tokens;
     mapping(uint256 => _Order) public orders;
+    uint256 public orderCount;
 
     //Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
-    event Withdraw(address token, address user, uint amount, uint balance);
+    event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 timestamp
+    );
 
+    //Structs
     struct _Order {
-        uint id;
+        uint256 id;
         address user;
         address tokenGet;
-        uint amountGet;
+        uint256 amountGet;
         address tokenGive;
-        uint timestamp;
+        uint256 timestamp;
     }
 
     constructor (address _feeAccount, uint256 _feePercent) public {
@@ -34,6 +44,9 @@ contract Exchange {
     function() external {
         revert();
     }
+
+
+    //Deposit & Withdraw
 
     function depositEther() payable public {
         tokens[ETHER][msg.sender] = tokens[ETHER][msg.sender].add(msg.value);
@@ -67,13 +80,16 @@ contract Exchange {
         return tokens[_token][_user];
     }
 
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(_id, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
 
 }
 
 
 
-
-//Deposit & Withdraw
 
 
 
