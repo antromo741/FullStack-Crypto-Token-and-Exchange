cb9d27ec7689d5f64f6378ef2c2982a5961bba60
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-02 16:51:56 -0400

    added emit an order event test

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -224,6 +224,27 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
         it('tracks the newly created order', async () => {
             const orderCount = await exchange.orderCount()
             orderCount.toString().should.equal('1')
+            const order = await exchange.orders('1')
+            order.id.toString().should.equal('1', 'id is correct')
+            order.user.should.equal(user1, 'user is correct')
+            order.tokenGet.should.equal(token.address, ' tokenGet is correct')
+            order.amountGet.toSting().should.equal(tokens(1).toSting(), 'amountGet is correct')
+            order.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+            order.amoountGive.toString().should.equal(ether(1).toString(), 'amoountGive is correct')
+            order.timestamp.toString().length.should.be.at.least(1), 'timestamp is preset'
+        })
+
+        ut('emits an "order" event', async () => {
+            const log = result.logs[0]
+            log.event.should.eq('Order')
+            const event = log.args
+            event.id.toSting().should.equal('1', 'id is correct')
+            event.user.should.equal(user1, 'user is correct')
+            event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+            event.amountGet.toString().should.equal(tokens(1).toString(), ' amountGet is correct')
+            event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+            event.aountGive.toString().should.equal(ether(1).toString(), ' amountGive is correct')
+            event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
         })
     })
 })
