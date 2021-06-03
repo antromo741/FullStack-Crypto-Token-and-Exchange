027a2da53b2f81e00001a3421d146917c780f0fc
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-03 12:00:25 -0400

    added order actions tests for canceling the orders and to test invalid cancel requests also fixed an error in token deposits

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -8,7 +8,7 @@ require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Exchange', ([deployer, feeAccount, user1]) => {
+contract('Exchange', ([deployer, feeAccount, user1, user2]) => {
     let token
     let exchange
     const feePercent = 10
@@ -147,13 +147,15 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
 
             it('fails when no tokens are approved', async () => {
 
-                it('rejects Ether deposits', async () => {
-                    await exchange.depositToken(ETHER_ADDRESS, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
-                })
+                
 
                 //Dont approve tokens before depositing
                 await exchange.depositToken(token.address, tokens(10), {from: user1 }).should.be.rejectedWith(EVM_REVERT)
 
+            })
+            
+            it('rejects Ether deposits', async () => {
+                    await exchange.depositToken(ETHER_ADDRESS, tokens(10), { from: user1 }).should.be.rejectedWith(EVM_REVERT)
             })
         })
     })
@@ -245,6 +247,61 @@ contract('Exchange', ([deployer, feeAccount, user1]) => {
             event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
             event.amountGive.toString().should.equal(ether(1).toString(), ' amountGive is correct')
             event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+        })
+
+        describe('order actions', async () => {
+            beforeEach(async () => {
+                //user1 deposits ether
+                await exchange.depositEther({ from : user1, value: ether(1) })
+                //user1 makes an order to buy tokens with ether
+                await exchange.makeOrder(token.address, tokens(1), ETHER_ADDRESS, ether(1), {from: user1 })
+            })
+
+            describe('canceling orders', async () => {
+                let result
+
+                describe('success', async () => {
+                    //cancel the order
+                    beforeEach(async () => {
+                        result = await exchange.cancelOrder('1', { from: user1 })
+                    })
+                    //updates to cancel
+                    it('updates cancelled orders', async () => {
+                        const orderCancelled = await exchange.orderCancelled(1)
+                        orderCancelled.should.equal(true)
+                    })
+
+                    it('emits an "Cancel" event', async () => {
+                        const log = result.logs[0]
+                        log.event.should.eq('Cancel')
+                        const event = log.args
+                        event.id.toString().should.equal('1', 'id is correct')
+                        event.user.should.equal(user1, 'user is correct')
+                        event.tokenGet.should.equal(token.address, 'tokenGet is correct')
+                        event.amountGet.toString().should.equal(tokens(1).toString(), ' amountGet is correct')
+                        event.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+                        event.amountGive.toString().should.equal(ether(1).toString(), ' amountGive is correct')
+                        event.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+                    })
+                })
+                
+                describe('failure', async () => {
+                    it('rejects invalid order ids', async () => {
+                        const invalidOrderId = 99999
+                        await exchange.cancelOrder(invalidOrderId, { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+                    })
+                    
+                    it('rejects unauthorized cancelations', async () => {
+                        //test another user tring to cancel
+                        await exchange.cancelOrder('1', { from: user2 }).should.be.rejectedWith(EVM_REVERT)
+                    })
+               
+                })
+
+
+            })
+
+
         })
     })
 })
