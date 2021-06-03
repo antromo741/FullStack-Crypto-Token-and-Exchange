0df87ea7fae162aec4bc5466025354d8f2831fc6
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-03 15:33:38 -0400

    created a trade function where we incorperate the fee to be decucted from one acct and added to the other, wrote out a test for filling orders, checking to make sure the ids and users match up

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -13,6 +13,7 @@ contract Exchange {
     mapping(uint256 => _Order) public orders;
     uint256 public orderCount;
     mapping(uint256 => bool) public orderCancelled;
+    mapping(uint256 => bool) public orderFilled;
 
     //Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
@@ -34,6 +35,17 @@ contract Exchange {
         uint256 amountGet,
         address tokenGive,
         uint256 amountGive,
+        uint256 timestamp
+    );
+
+    event Trade(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        address userFill,
         uint256 timestamp
     );
 
@@ -109,25 +121,25 @@ contract Exchange {
     }
 
     function fillOrder(uint256 _id) public {
+        require(_id > 0 && _id <= orderCount);
+        require(!orderFilled[_id]);
+        require(!orderCancelled[_id]);
         _Order storage _order = orders[_id];
-        _trade(_order.id, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
-
-
-        
+        _trade(_order.id, _order.user, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive);
+        orderFilled[_order.id] = true;
     }
 
     function _trade(uint256 _orderId, address _user, address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) internal{
-       //fee paid by user who fills order aka msg sender
-       //fee deducted from _amountGet
-        uint256 _feeAmount = _amountGive.mul(feePercent).div(100);
-        
-        //user creates the order and msg sender fills the order
-        tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
-        tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
-        //adds fees
-        tokens[_tokenGive][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
-        tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
-        tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+        // Fee paid by the user that fills the order, a.k.a. msg.sender.
+    uint256 _feeAmount = _amountGet.mul(feePercent).div(100);
+
+    tokens[_tokenGet][msg.sender] = tokens[_tokenGet][msg.sender].sub(_amountGet.add(_feeAmount));
+    tokens[_tokenGet][_user] = tokens[_tokenGet][_user].add(_amountGet);
+    tokens[_tokenGet][feeAccount] = tokens[_tokenGet][feeAccount].add(_feeAmount);
+    tokens[_tokenGive][_user] = tokens[_tokenGive][_user].sub(_amountGive);
+    tokens[_tokenGive][msg.sender] = tokens[_tokenGive][msg.sender].add(_amountGive);
+
+    emit Trade(_orderId, _user, _tokenGet, _amountGet, _tokenGive, _amountGive, msg.sender, now);
     }
 
 }
