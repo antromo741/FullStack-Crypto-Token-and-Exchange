efcf8053c8e074166ae954f283ddf91146e73a43
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-07 14:21:11 -0400

    added the blockchain data and can read what network im on in the console this is so much fun

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { Component } from 'react';
 import './App.css';
+import Web3 from 'web3';
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-     <p>Romo</p>
-    </div>
-  );
+class App extends Component {
+
+  componentWillMount() {
+     this.loadBlockchainData()
+  }
+
+  async loadBlockchainData() {
+    const web3 = new Web3(window.ethereum);
+    const network = await web3.eth.net.getNetworkType()
+    console.log("network", network)
+  }
+
+  render() {
+    return (
+      <div className="App">
+      <h1>Hello</h1>
+      <p>Romo</p>
+      </div>
+    );
+  }
 }
 
 export default App;
