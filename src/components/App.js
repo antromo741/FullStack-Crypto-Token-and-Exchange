fcff278033fda377e8681e22719f6b49ec378f11
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-07 16:59:21 -0400

    made a bunch of console logs in app.js to show the connectons

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Web3 from 'web3';
+import Token from '../abis/Token.json'
 
 class App extends Component {
 
@@ -11,7 +12,15 @@ class App extends Component {
   async loadBlockchainData() {
     const web3 = new Web3(window.ethereum);
     const network = await web3.eth.net.getNetworkType()
+    const accounts = await web3.eth.getAccounts()
+    const abi = Token.abi
+    const networks = Token.networks
+    const networkId = await web3.eth.net.getId()
+    console.log("accounts", accounts)
     console.log("network", network)
+    console.log("abi", abi)
+    console.log("networks", networks)
+    console.log("network-data", Token.networks[networkId])
   }
 
   render() {
