7d076126beb29a438d115ef65af97fbf0ebb8166
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-08 10:34:49 -0400

    added an action file with a load web3 action and an interactiosn file to do the trigger the event

---
+++ src/store/interactions.js
@@ -0,0 +1,12 @@
+
+
+export const loadWeb3 = async (dispatch) => {
+    if (typeof window.ethereum !== 'undefined') {
+        const web3 = new Web3(window.ethereum)
+        dispatch(web3Loaded(web3))
+        return web3
+    } else {
+        window.alert('Please install MetaMask')
+        window.location.assign("https://metamask.io/")
+    }
+}
