3975e2faa82ae1f8f6985863b869866fbc2b0878
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-09 14:12:17 -0400

    added a card with the trades table to start, also fixed spelling error causing odd render

--- src/components/Trades.js
+++ src/components/Trades.js
@@ -3,24 +3,27 @@ import { connect } from 'react-redux'
 
 class Trades extends Component {
     render() {
-        return ( 
-            <div className="card bg-dark text-white">
-                <div className="card-header">
-                Some title
+        return (
+            <div className="vertical">
+                <div className="card bg-dark text-white">
+                    <div className="card-header">
+                        Trades
+          </div>
+                    <div className="card-body">
+                        <table className="table table-dark table-sm small">
+                            <thead>
+                                <tr>
+                                    <th>Time</th>
+                                    <th>ROM</th>
+                                    <th>ROM/ETH</th>
+                                </tr>
+                            </thead>
+                            
+                        </table>
+                    </div>
                 </div>
-            <div className="card-body">
-
-
-                
             </div>
-
-
-            </div>
-
-
         )
-
-        
     }
 }
 
