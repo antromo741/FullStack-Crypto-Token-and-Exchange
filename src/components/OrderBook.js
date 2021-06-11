f9a523225a9e62feefbd6d5a2ee0cd948774c853
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-11 14:54:03 -0400

    added two functions to show the orders in order book renderOrder and show orderBook

--- src/components/OrderBook.js
+++ src/components/OrderBook.js
@@ -4,10 +4,36 @@ import {
     orderBookSelector,
     orderBookLoadedSelector
 } from '../store/selectors'
+import Spinner from './Spinner'
+
+const renderOrder = (order) => {
+    return(
+        <tr key={order.id}>
+            <td>{order.tokenAmount}</td>
+            <td className={`text-${order.orderTypeClass}`}>{order.tokenPrice}</td>
+            <td>{order.etherAmount}</td>
+        </tr>
+    )
+}
+
+const showOrderBook = (props) => {
+    const { orderBook } = props
+    return(
+        <tbody>
+            {orderBook.sellOrders.map((order) => renderOrder(order))}
+            <tr>
+                <th>ROM</th>
+                <th>ROM/ETH</th>
+                <th>ETH</th>
+            </tr>
+            {orderBook.buyOrders.map((order) => renderOrder(order))}
+        </tbody>
+    )
+}
+
 
 class OrderBook extends Component {
     render() {
-        console.log(this.props.showOrderBook, this.props.orderBook)
         return (
             <div className="vertical">
                 <div className="card bg-dark text-white">
@@ -16,7 +42,7 @@ class OrderBook extends Component {
                     </div>
                     <div className="card-body order-book">
                         <table className="table table-dark table-sm small">
-
+                        {this.props.showOrderBook ? showOrderBook(this.props) : <Spinner type='table' /> }
                         </table>
                     </div>
                  </div>
