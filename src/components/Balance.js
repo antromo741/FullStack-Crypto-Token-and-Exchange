33eb0468782978c40db79a80499368cd62a36df7
Anthony_Romagnolo <anthonyroma741@gmail.com>
2021-06-16 13:47:32 -0400

    added the showform which like the other components will only render if the data is loaded, we have our balances loaded, and several tables to show the account balance

--- src/components/Balance.js
+++ src/components/Balance.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { loadBalances } from '../store/interactions'
+import { Tabs, Tab } from 'react-bootstrap'
+import Spinner from './Spinner'
 import {
     web3Selector, 
     exchangeSelector,
@@ -13,6 +15,53 @@ import {
     exchangeTokenBalanceSelector,
 
 } from '../store/selectors'
+
+
+
+const showForm = (props) => {
+    const { 
+        etherBalance,
+        exchangeEtherBalance,
+        tokenBalance, 
+        exchangeTokenBalance,
+    } = props
+
+    return(
+        <Tabs defaultActiveKey="deposit" className="bg-dark text-white">
+            <Tab eventKey="deposit" title="Deposit" className="bg-dark">
+                <table className="table table-dark table-sm small">
+                    <thead>
+                        <tr>
+                            <th>Token</th>
+                            <th>Wallet</th>
+                            <th>Exchange</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>ETH</td>
+                            <td>{etherBalance}</td>
+                            <td>{exchangeEtherBalance}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <table className="table table-dark table-sm small">
+                    <tbody>
+                        <tr>
+                            <td>ROM</td>
+                            <td>{tokenBalance}</td>
+                            <td>{exchangeTokenBalance}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </Tab>
+
+            <Tab eventKey="withdraw" title="Withdraw" className="bg-dark">
+
+            </Tab>
+        </Tabs>
+    )
+}
 
 class Balance extends Component {
     componentWillMount() {
@@ -31,13 +80,15 @@ class Balance extends Component {
                 Balance
                 </div>
             <div className="card-body">
-
+        {this.props.showForm ? showForm(this.props) : <Spinner /> }
             </div>
         </div>
     )
 }
 }
 function mapStateToProps(state) {
+    const balancesLoading = balancesLoadingSelector(state)
+
     return{
         account: accountSelector(state),
         exchange: exchangeSelector(state),
@@ -47,7 +98,9 @@ function mapStateToProps(state) {
         tokenBalance: tokenBalanceSelector(state),
         exchangeEtherBalance: exchangeEtherBalanceSelector(state),
         exchangeTokenBalance: exchangeTokenBalanceSelector(state),
-        balncesLoading: balancesLoadingSelector(state),
+        balancesLoading: balancesLoadingSelector(state),
+        balancesLoading,
+        showForm: !balancesLoading
     }
 }
 
